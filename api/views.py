e912309fb4657b422285ff3d172015dfcfdbcacd
Damian <zettie008@gmail.com>
2020-04-19 23:44:07 +0100

    modified views.py

--- api/views.py
+++ api/views.py
@@ -11,11 +11,15 @@ def index(request):
 
 
 def who_am_i(request):
-    ipaddress = request.META.get('REMOTE_ADDR')
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
     language = request.META.get('HTTP_ACCEPT_LANGUAGE')
     user_agent = request.META.get('HTTP_USER_AGENT')
     return JsonResponse({
-        'ipaddress': ipaddress,
+        'ipaddress': ip,
         'language': language,
         'software': user_agent
     })
